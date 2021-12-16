263b732efac21bd5c97da6ed2d409d807e5e570a
cyj0225 <cyj0225@gsretail.com>
2021-12-16 11:13:44 +0900

    'search'

--- src/app.jsx
+++ src/app.jsx
@@ -1,9 +1,28 @@
 import { useEffect, useState } from 'react';
-import './app.css';
+import styles from './app.module.css';
+import SearchHeader from './components/search_header/search_header';
 import VideoList from './components/video_list/video_list';
 
 function App() {
   const [videos, setVideos] = useState([]);
+
+  const search = query => {
+    const requestOptions = {
+      method: 'GET',
+      redirect: 'follow',
+    };
+
+    fetch(
+      `https://www.googleapis.com/youtube/v3/search?type=video&part=snippet&maxResults=25&q=${query}&key=AIzaSyBAvtV-6Mb6rYhbCzF1omh9Der3ZSoMdOQ`,
+      requestOptions
+    )
+      .then(response => response.json())
+      .then(result =>
+        result.items.map(item => ({ ...item, id: item.id.videoId }))
+      )
+      .then(items => setVideos(items))
+      .catch(error => console.log('error', error));
+  };
 
   useEffect(() => {
     console.log('useEffect');
@@ -14,14 +33,19 @@ function App() {
     };
 
     fetch(
-      'https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=bts&key=AIzaSyBAvtV-6Mb6rYhbCzF1omh9Der3ZSoMdOQ',
+      'https://www.googleapis.com/youtube/v3/search?type=video&part=snippet&maxResults=25&q=bts&key=AIzaSyBAvtV-6Mb6rYhbCzF1omh9Der3ZSoMdOQ',
       requestOptions
     )
       .then(response => response.json())
       .then(result => setVideos(result.items))
       .catch(error => console.log('error', error));
   }, []);
-  return <VideoList videos={videos} />;
+  return (
+    <div className={styles.app}>
+      <SearchHeader onSearch={search} />
+      <VideoList videos={videos} />;
+    </div>
+  );
 }
 
 export default App;
