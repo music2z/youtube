dbb3e96f359c552fb0a41cc96fe40ff9fbfd9348
cyj0225 <cyj0225@gsretail.com>
2021-12-16 10:36:29 +0900

    first commit

--- src/app.jsx
+++ src/app.jsx
@@ -1,9 +1,27 @@
+import { useEffect, useState } from 'react';
 import './app.css';
+import VideoList from './components/video_list/video_list';
 
 function App() {
-  return (
-    <h1>Hello :)</h1>
-  );
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    console.log('useEffect');
+
+    const requestOptions = {
+      method: 'GET',
+      redirect: 'follow',
+    };
+
+    fetch(
+      'https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=bts&key=AIzaSyBAvtV-6Mb6rYhbCzF1omh9Der3ZSoMdOQ',
+      requestOptions
+    )
+      .then(response => response.json())
+      .then(result => setVideos(result.items))
+      .catch(error => console.log('error', error));
+  }, []);
+  return <VideoList videos={videos} />;
 }
 
 export default App;
