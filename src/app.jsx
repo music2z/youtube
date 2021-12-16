128832382cd2c4840b6ebf07e8338551e05c72ed
cyj0225 <cyj0225@gsretail.com>
2021-12-16 15:51:29 +0900

    프로젝트 커밋

--- src/app.jsx
+++ src/app.jsx
@@ -1,49 +1,49 @@
-import { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import styles from './app.module.css';
-import SearchHeader from './components/search_header/search_header';
 import VideoList from './components/video_list/video_list';
+import SearchHeader from './components/search_header/search_header';
+import VideoDetail from './components/video_detail/video_detail';
 
-function App() {
+function App({ youtube }) {
   const [videos, setVideos] = useState([]);
+  const [selectedVideo, setSelectedVideo] = useState(null);
 
-  const search = query => {
-    const requestOptions = {
-      method: 'GET',
-      redirect: 'follow',
-    };
-
-    fetch(
-      `https://www.googleapis.com/youtube/v3/search?type=video&part=snippet&maxResults=25&q=${query}&key=AIzaSyBAvtV-6Mb6rYhbCzF1omh9Der3ZSoMdOQ`,
-      requestOptions
-    )
-      .then(response => response.json())
-      .then(result =>
-        result.items.map(item => ({ ...item, id: item.id.videoId }))
-      )
-      .then(items => setVideos(items))
-      .catch(error => console.log('error', error));
+  const selectVideo = video => {
+    setSelectedVideo(video);
   };
 
+  const search = useCallback(
+    query => {
+      setSelectedVideo(null);
+      youtube
+        .search(query) //
+        .then(videos => setVideos(videos));
+    },
+    [youtube]
+  );
+
   useEffect(() => {
-    console.log('useEffect');
-
-    const requestOptions = {
-      method: 'GET',
-      redirect: 'follow',
-    };
-
-    fetch(
-      'https://www.googleapis.com/youtube/v3/search?type=video&part=snippet&maxResults=25&q=bts&key=AIzaSyBAvtV-6Mb6rYhbCzF1omh9Der3ZSoMdOQ',
-      requestOptions
-    )
-      .then(response => response.json())
-      .then(result => setVideos(result.items))
-      .catch(error => console.log('error', error));
-  }, []);
+    youtube
+      .mostPopular() //
+      .then(videos => setVideos(videos));
+  }, [youtube]);
   return (
     <div className={styles.app}>
       <SearchHeader onSearch={search} />
-      <VideoList videos={videos} />;
+      <section className={styles.content}>
+        {selectedVideo && (
+          <div className={styles.detail}>
+            <VideoDetail video={selectedVideo} />
+          </div>
+        )}
+        <div className={styles.list}>
+          <VideoList
+            videos={videos}
+            onVideoClick={selectVideo}
+            display={selectedVideo ? 'list' : 'grid'}
+          />
+        </div>
+      </section>
     </div>
   );
 }
